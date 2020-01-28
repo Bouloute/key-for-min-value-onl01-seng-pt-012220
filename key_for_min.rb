50b0c58cd172186b4c73189e4f29ef47238931ca
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-28 02:44:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
-
+  #initialise
+  puts name_hash[0]
+  binding.pry
 end
+
+key_for_min_value(["one" => 1, "two" => 2])
