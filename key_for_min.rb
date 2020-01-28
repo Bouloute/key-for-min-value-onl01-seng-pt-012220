81f9f7d714ff763e7f65da21394f8a9a4b2031eb
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-28 02:58:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,12 @@ require 'pry'
 def key_for_min_value(name_hash)
   #initialise
   result, min = name_hash[0].first
-  binding.pry
+
+  name_hash.each{|item, price|
+    if price < min 
+      result, min = item, price
+    end
+  }
 end
 
 key_for_min_value(["one" => 1, "two" => 2])
